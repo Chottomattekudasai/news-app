35978885f4d2b32bc2c9b6e93b6b679e7fa7960a
ainso <ainsofgorn@gmail.com>
2022-02-02 18:19:05 +0300

    main page appeared

--- NewsApp/MainApp/views.py
+++ NewsApp/MainApp/views.py
@@ -1,15 +1,13 @@
 import requests
 from django.shortcuts import render
 
-
 def index(request):
     search = request.GET.get('search')
     if search is None:
-        keyword = 'steam'
+        url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=c34769406bda439c911f3d1749555ef7'
     else:
         keyword = search
-
-    url = 'https://newsapi.org/v2/everything?q=' + keyword + '&apiKey=c34769406bda439c911f3d1749555ef7'
+        url = 'https://newsapi.org/v2/everything?q=' + keyword + '&apiKey=c34769406bda439c911f3d1749555ef7'
 
     news = requests.get(url).json()
     a = news['articles']
