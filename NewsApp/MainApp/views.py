c53ed70962e34cc7c312de28f093651ca7354b8d
ainso <ainsofgorn@gmail.com>
2022-02-02 16:43:59 +0300

    Initial commit

---
+++ NewsApp/MainApp/views.py
@@ -0,0 +1,30 @@
+import requests
+from django.shortcuts import render
+
+
+def index(request):
+    search = request.GET.get('search')
+    if search is None:
+        keyword = 'steam'
+    else:
+        keyword = search
+
+    url = 'https://newsapi.org/v2/everything?q=' + keyword + '&apiKey=c34769406bda439c911f3d1749555ef7'
+
+    news = requests.get(url).json()
+    a = news['articles']
+    desc = []
+    title = []
+    img = []
+
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+
+    my_list = zip(title, desc, img)
+
+    context = {'my_list': my_list}
+
+    return render(request, 'index.html', context)
